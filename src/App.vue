7d281bd18bf7022eb4d322b7555d1c5e1c748a1f
itHy <1324876445@qq.com>
2022-07-03 11:15:05 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
+      |
       <router-link to="/about">About</router-link>
     </nav>
     <router-view />
@@ -16,14 +17,11 @@
   text-align: center;
   color: #2c3e50;
 }
-
 nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
-
     &.router-link-exact-active {
       color: #42b983;
     }
